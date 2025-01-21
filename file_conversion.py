bfafcdf5e49826acf02405e6ff8201700bab388c
nairpratheesh <pratheesh_pk@yahoo.com>
2025-01-21 15:57:16 +0000

    Sorting csv file

---
+++ file_conversion.py
@@ -0,0 +1,17 @@
+import pandas as pd
+try:
+    from urllib.request import Request, urlopen  # Python 3
+except ImportError:
+    from urllib2 import Request, urlopen  # Python 2
+
+req = Request('https://coderbyte.com/api/challenges/logs/user-info-csv')
+req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
+content = urlopen(req)
+
+df = pd.read_csv(content)
+print("before sorting",df)
+df.sort_values(["Name"],axis=0,ascending=[True],inplace=True)
+# data = pd.read_csv('C:\\Users\\prath\\PycharmProjects\\June5\\sort.txt')
+print("After converting",df)
+print(df.to_json())
+
